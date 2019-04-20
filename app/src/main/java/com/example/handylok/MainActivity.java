50b0b24790848b1038e9fee21c8c0d80e09ce2d7
uchan <uchanism24@naver.com>
2019-04-20 20:54:31 +0900

    DBAdapter/WriteActivity 에 맞춘 MainActivity 업데이트

--- app/src/main/java/com/example/handylok/MainActivity.java
+++ app/src/main/java/com/example/handylok/MainActivity.java
@@ -1,83 +1,171 @@
 package com.example.handylok;
 
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AlertDialog;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.DatePicker;
-import android.widget.EditText;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
-
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Calendar calendar;
-    int Year, Month, Day;
-    int Hour, Minute;
-    TextView date;
-
+    final Context context = this;
     private final long FINISH_INTERVAL_TIME = 2000;
     private long backPressedTime = 0;
+
+    CustomAdapter listAdapter;
+    DBAdapter db;
+    boolean dbOpen;
+    Cursor currentCursor;
+    int nowIndex;
+
+    private final static int addRequestCode = 100;
+    private final static int modifyRequestCode = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Init
-        Button add = findViewById(R.id.add);
+        // make and open database
+        db = new DBAdapter(context);
+        db.open();
+        dbOpen = true;
 
-        // Btn_Click_Listener
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 버튼이 클릭되면 현재 날짜/시간 불러오기
-                show_add();
-//                calendar = Calendar.getInstance();
-//                Year = calendar.get(Calendar.YEAR) ;
-//                Month = calendar.get(Calendar.MONTH);
-//                Day = calendar.get(Calendar.DAY_OF_MONTH);
-//                Hour = calendar.get(calendar.HOUR_OF_DAY);
-//                Minute = calendar.get(calendar.MINUTE);
-//                date = findViewById(R.id.date);
-//                DatePickerDialog d_dialog = new DatePickerDialog(MainActivity.this, mDataSetListener, Year, Month, Day);
-//                d_dialog.show();
-//                Intent intent = new Intent(getApplicationContext(), WriteActivity.class);
-//                startActivity(intent);
-            }
-        });
+        // get column index
+        currentCursor = db.fetchAllData();
 
+        // setup listview
+        ListView listView = findViewById(R.id.listView);
+        listAdapter = new CustomAdapter(this);
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(itemClickListener);
+        listView.setOnItemLongClickListener(itemLongClickListener);
     }
 
-    // DatePickerDialog
-    private DatePickerDialog.OnDateSetListener mDataSetListener = new DatePickerDialog.OnDateSetListener() {
+    protected void onResume() {
+        super.onResume();
+
+        if (!dbOpen) {
+            db.open();
+            dbOpen = true;
+        }
+        currentCursor = db.fetchAllData();
+        listAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (dbOpen) {
+            db.close();
+            dbOpen = false;
+        }
+    }
+
+    // Button Click Listner
+    public void clickAdd(View view) {
+        startActivityForResult(new Intent(context, WriteActivity.class).putExtra("MainRequestCode", addRequestCode), addRequestCode);
+    }
+
+    // List Item Click Listener
+    AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
         @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-            TimePickerDialog t_dialog = new TimePickerDialog(MainActivity.this, mTimeSetListener, Hour, Minute, true);
-            t_dialog.show();
-            date.setText(year + "/" + (monthOfYear + 1) + "/" + dayOfMonth + " ");
+        public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+            currentCursor.moveToPosition(position);
+            nowIndex = currentCursor.getInt(0);
+            String name = String.valueOf(currentCursor.getString(1));
+            String place = String.valueOf(currentCursor.getString(2));
+            String date = String.valueOf(currentCursor.getString(3));
+            String contexts = String.valueOf(currentCursor.getString(4));
+
+            Intent intent = new Intent(context, WriteActivity.class);
+            intent.putExtra("_id", nowIndex);
+            intent.putExtra("name", name);
+            intent.putExtra("place", place);
+            intent.putExtra("date", date);
+            intent.putExtra("contexts", contexts);
+            intent.putExtra("MainRequestCode", modifyRequestCode);
+
+            startActivityForResult(intent, modifyRequestCode);
         }
     };
 
-    // TimePickerDialog
-    private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+    // List Item Long Click Listener
+    AdapterView.OnItemLongClickListener itemLongClickListener = new AdapterView.OnItemLongClickListener() {
         @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            date.append(hourOfDay + "시 " + minute + "분");
+        public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+            Toast.makeText(context, "삭제함", Toast.LENGTH_SHORT).show();
+            currentCursor.moveToPosition(position);
+            nowIndex = currentCursor.getInt(0); // id 열
+            db.delData(nowIndex);
+            currentCursor = db.fetchAllData();
+            listAdapter.notifyDataSetChanged();
+            return true; // 다음 이벤트 계속 진행 false, 이벤트 완료 true
         }
     };
+
+    public class CustomAdapter extends BaseAdapter {
+        Context my_context;
+
+        CustomAdapter(Context context) {
+            my_context = context;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public int getCount() {
+            return currentCursor.getCount();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ListViewHolder viewHolder;
+
+            // 캐시된 뷰가 없을 경우 새로 생성
+            if (convertView == null) { //Scroll in
+                LayoutInflater inflater = (LayoutInflater) my_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.list_row, parent, false);
+
+                viewHolder = new ListViewHolder();
+                viewHolder.nameView = convertView.findViewById(R.id.nameView);
+                viewHolder.dateView = convertView.findViewById(R.id.dateView);
+                convertView.setTag(viewHolder);
+                // 캐시된 뷰가 있을 경우 저장된 뷰홀더 사용
+            } else { //Scroll out
+                // findViewById를 이용하지 않기위해 getTag, setTag 이용
+                viewHolder = (ListViewHolder)convertView.getTag();
+            }
+
+            currentCursor.moveToPosition(position);
+            viewHolder.nameView.setText(currentCursor.getString(1)); // 이름 열
+            viewHolder.dateView.setText(currentCursor.getString(3)); // 일시 열
+
+            return convertView;
+        }
+
+        private class ListViewHolder {
+            TextView nameView;
+            TextView dateView;
+        };
+    }
 
     // back key 2번 종료
     @Override
@@ -92,94 +180,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "뒤로 가기 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
         }
     }
-
-    /* add 다이얼로그 */
-    void show_add() {
-        /* dialog_login */
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
-        View view = inflater.inflate(R.layout.activity_write, null);
-        builder.setView(view);
-
-//        final Button submit = (Button) view.findViewById(R.id.buttonSubmit);
-//        final Button register = (Button) view.findViewById(R.id.buttonRegister);
-//        final EditText login_idText = (EditText) view.findViewById(R.id.idText);
-//        final EditText login_passwordText = (EditText) view.findViewById(R.id.passwordText);
-//        final CheckBox autoLoginCheck = (CheckBox) view.findViewById(R.id.autoLoginCheck);
-//        final CheckBox saveID = (CheckBox) view.findViewById(R.id.saveID);
-
-//        SharedPreferences id = getSharedPreferences("save", MODE_PRIVATE);
-//        login_idText.setText(id.getString("userID", ""));
-//        saveID.setChecked(id.getBoolean("saveIDCheck", false));
-
-        final AlertDialog dialog = builder.create();
-
-        dialog.show();
-
-//        submit.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                userID = login_idText.getText().toString();
-//                userPassword = login_passwordText.getText().toString();
-//
-//                Response.Listener<String> responseListener = new Response.Listener<String>() {
-//
-//                    @Override
-//                    public void onResponse(String response) {
-//                        try {
-//                            JSONObject jsonResponse = new JSONObject(response);
-//                            boolean success = jsonResponse.getBoolean("success");
-//                            if (success) {
-//                                dialog.dismiss();
-//                                Intent intent = new Intent(context, MainActivity.class);
-//
-//                                userID = jsonResponse.getString("userID");
-//                                userPassword = jsonResponse.getString("userPassword");
-//                                userName = jsonResponse.getString("userName");
-//                                userAge = jsonResponse.getInt("userAge");
-//
-//                                /* SharedPreference */
-//                                SharedPreferences id = getSharedPreferences("save", MODE_PRIVATE);
-//                                SharedPreferences.Editor saveID_info = id.edit();
-//
-//                                자동로그인 체크 이벤트
-//                                if (autoLoginCheck.isChecked()) {
-//                                    autoCheck++;
-//                                    savePreference();
-//                                } else {
-//                                    intent.putExtra("userID", userID);
-//                                    intent.putExtra("userPassword", userPassword);
-//                                    intent.putExtra("userName", userName);
-//                                    intent.putExtra("userAge", userAge);
-//                                }
-//                                if (saveID.isChecked()) {
-//                                    saveID_info.putString("userID", userID);
-//                                    saveID_info.putBoolean("saveIDCheck", true);
-//                                    saveID_info.commit();
-//                                } else {
-//                                    saveID_info.putString("userID", null);
-//                                    saveID_info.putBoolean("saveIDCheck", false);
-//                                    saveID_info.commit();
-//                                }
-//
-//                                finish();
-//                                startActivity(intent);
-//                            } else {
-//                                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-//                                builder.setMessage("로그인에 실패하였습니다.")
-//                                        .setNegativeButton("다시 시도", null)
-//                                        .create()
-//                                        .show();
-//                            }
-//                        } catch (Exception e) {
-//                            e.printStackTrace();
-//                        }
-            }
-//        });
-//                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
-//                RequestQueue queue = Volley.newRequestQueue(context);
-//                queue.add(loginRequest);
-//            }
-//        });
-//
 
 }
